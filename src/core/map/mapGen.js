367f9ebff13d0a2e4974dd072e42dc1fc83edf2f
mattstyles <matt@veryfizzyjelly.com>
2015-06-19 09:54:28 +0100

    #add game config for constants

---
+++ src/core/map/mapGen.js
@@ -0,0 +1,50 @@
+
+import DisplacementMap from 'displacement-map'
+
+import config from 'config/gameConf'
+import { to1d } from 'core/utils/maths'
+
+const SIZE = config.getIn( [ 'map', 'chunkSize' ] )
+
+
+class MapGen {
+    constructor() {
+        this.generator = new DisplacementMap()
+    }
+
+    createChunk() {
+        let genSize = SIZE + 1
+        let buf = new ArrayBuffer( genSize * genSize )
+        let buf8 = new Uint8Array( buf )
+
+        // Seed corners
+        buf8[ 0 ] = 0x80
+        buf8[ genSize - 1 ] = 0x80
+        buf8[ ( genSize * genSize ) - genSize ] = 0x80
+        buf8[ ( genSize * genSize ) - 1 ] = 0x80
+
+        return new Promise( ( resolve, reject ) => {
+            this.generator.generate({
+                map: buf8,
+                width: genSize,
+                height: genSize
+            })
+                .then( map => {
+                    // Generation requires n^2 + 1, the chunk should be ^2
+                    let chunk = new Uint8Array( SIZE * SIZE )
+                    for ( let x = 0; x < SIZE - 1; x++ ) {
+                        for ( let y = 0; y < SIZE - 1; y++ ) {
+                            chunk[ to1d( x, y, SIZE - 1 ) ] = map[ to1d( x, y, SIZE ) ]
+                        }
+                    }
+
+                    resolve( chunk )
+                })
+                .catch( reject )
+        })
+
+    }
+
+}
+
+export default new MapGen()
