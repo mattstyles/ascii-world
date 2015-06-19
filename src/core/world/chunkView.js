e5817b1de3f52747521669523eeb000b4a2b98a1
mattstyles <matt@veryfizzyjelly.com>
2015-06-19 22:12:22 +0100

    #update refactor base chunkView

--- src/core/world/chunkView.js
+++ src/core/world/chunkView.js
@@ -1,8 +1,5 @@
 
 import { to1d, Point } from 'core/utils/maths'
-import config from 'config/gameConf'
-
-const CHUNK_SIZE = config.getIn( [ 'map', 'chunkSize' ] )
 
 /**
  * Responsible for rendering a whole chunk
@@ -37,6 +34,15 @@ export default class ChunkView {
         return texture[ ~~( Math.random() * texture.length ) ]
     }
 
+    getFillStyle( cell ) {
+        return 'rgba( 96, 64, 64, ' + ( cell / 0xff ) + ' )'
+    }
+
+    renderCell( cell, position ) {
+        this.ctx.fillStyle = this.getFillStyle( cell )
+        this.ctx.fillText( this.getTexture(), position.x, position.y )
+    }
+
     /**
      * @param params <Object>
      *   @param chunk <Uint8Array> raw chunk positional data
@@ -44,19 +50,14 @@ export default class ChunkView {
      *   @param position <Point> top-left point to render to
      */
     render( params ) {
-        console.log( 'Chunk:render', params )
         // For each cell defined by the renderRect
         // Create a fill style and position the text representing the entity
         // @TODO drawRect if a background style is needed for characters
         for( let y = params.renderRect.from.y; y <= params.renderRect.to.y; y++ ) {
             for( let x = params.renderRect.from.x; x <= params.renderRect.to.x; x++ ) {
-                let opacity = params.chunk[ to1d( x, y, params.renderRect.width ) ] / 0xff
-                this.ctx.fillStyle = 'rgba( 96, 64, 64, ' + opacity + ' )'
-
-                // @TODO simplify
-                let posX = ( params.position.x * this.cellDef.width ) + ( x * this.cellDef.width )
-                let posY = ( params.position.y * this.cellDef.height ) + ( y * this.cellDef.height )
-                this.ctx.fillText( this.getTexture(), posX, posY )
+                let posX = ( params.position.x + x ) * this.cellDef.width
+                let posY = ( params.position.y + y ) * this.cellDef.height
+                this.renderCell( params.chunk[ to1d( x, y, params.renderRect.width ) ], new Point( posX, posY ) )
             }
         }
     }
